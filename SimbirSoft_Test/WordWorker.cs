3518770b83a18aaff98a6773a52c6892609f6165
nyaami-pip <44361473+nyaami-pip@users.noreply.github.com>
2021-06-18 13:06:19 +0400

    Add files via upload

---
+++ SimbirSoft_Test/WordWorker.cs
@@ -0,0 +1,69 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimbirSoft_Test
+{
+    class WordWorker
+    {
+        public string[] site { get; private set; }
+
+        public WordWorker(string[] sites)
+        {
+            site = sites;
+        }
+
+        public void Work()
+        {
+            for(int i = 0; i < site.Length; i++) 
+            {
+                Console.WriteLine("Words of - " + site[i]);
+                Console.WriteLine("//////////////////////////////");
+                string text = ReadTextFromSite(site[i]);
+                
+                if(text == null) 
+                {
+                    Console.WriteLine("Bad respons from site, may be wrong");
+                    break;
+                }
+                var result = Calc(text);
+                result.Remove("");
+                foreach (var pair in result)
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+                Console.WriteLine("//////////////////////////////");
+            }
+                
+        }
+
+        static Dictionary<string, int> Calc(string site)
+        {
+            var res = new Dictionary<string, int>();
+
+            foreach (var word in site.Split
+                (' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t').Skip(1)) 
+            {
+                var count = 0;
+                res.TryGetValue(word, out count);
+                res[word] = count + 1;
+            }
+
+            return res;
+        }
+
+        public string ReadTextFromSite(string site)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+            try 
+            {
+                HtmlAgilityPack.HtmlDocument document = htmlWeb.Load(site);
+                return document.DocumentNode.InnerText;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Error : " + e.ToString());
+                return null;
+            }
+        }
+    }
+}
