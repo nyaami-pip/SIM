756f57e83ca07e646a02c46de7dd298903900296
nyaami-pip <44361473+nyaami-pip@users.noreply.github.com>
2021-06-18 13:06:09 +0400

    Add files via upload

---
+++ SimbirSoft_Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace SimbirSoft_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Press any key to Start");
+            Console.ReadKey();
+            Program program = new Program();
+            program.Start();
+            Console.WriteLine("Press any key to Escape");
+            Console.ReadKey();
+        }
+        public void Start()
+        {
+            FileWorker fileWorker = new FileWorker();
+            HTMLWorker htmlWorker;
+            WordWorker wordWorker;
+            var urldata = fileWorker.ReadFile();
+            Console.WriteLine(urldata);
+            htmlWorker = new HTMLWorker(urldata);
+            htmlWorker.SaveHTMLPages();
+            wordWorker = new WordWorker(urldata);
+            wordWorker.Work();
+        }
+    }
+}
