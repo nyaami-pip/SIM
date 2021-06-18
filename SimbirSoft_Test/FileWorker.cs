98c8e52adeb24baa610e5616585bb87e9739fd87
nyaami-pip <44361473+nyaami-pip@users.noreply.github.com>
2021-06-18 13:05:39 +0400

    Add files via upload

---
+++ SimbirSoft_Test/FileWorker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SimbirSoft_Test
+{
+    public class FileWorker
+    {
+        public string[] ReadFile()
+        {
+            string[] readText;
+            string[] tempReadText;
+           
+            string directory = Directory.GetCurrentDirectory();
+            string site = "sites.txt";
+            string path = directory + @"\" + site;
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File sites not exists, ");
+                string[] createText = { "https://www.google.com/", "https://yandex.com/", "https://www.cnet.com/" };
+                File.WriteAllLines(path, createText);
+            }
+
+            tempReadText = File.ReadAllLines(path);
+            readText = CheckUrl(tempReadText);
+            if (readText == null)
+            {
+                System.Environment.Exit(-1);
+            }
+            return readText;
+        }
+
+        private string[] CheckUrl(string[] checkurl)
+        {
+            List<string> tempReadTextCheck = new List<string>();
+            string pattern = @"^(http|http(s)?://)+([\w-]+\.)+(\[\?%&=]*)?";
+            for (int i = 0; i < checkurl.Length; i++)
+            {
+                if (Regex.IsMatch(checkurl[i], pattern, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("url подтвержден");
+                    tempReadTextCheck.Add(checkurl[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный url");
+                    Console.WriteLine("Строка {0}", checkurl[i]);
+                }
+            }
+            return tempReadTextCheck.ToArray();
+        }
+    }
+}
---
+++ SimbirSoft_Test/HTMLWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SimbirSoft_Test
+{
+    public class HTMLWorker
+    {
+        public string[] site { get; private set; }
+
+        public HTMLWorker(string[] sites)
+        {
+            site = sites;
+        }
+
+        public void SaveHTMLPages()
+        {
+            for (int i = 0; i < site.Length; i++) {
+                try 
+                {
+                    using (WebClient client = new WebClient()) {
+                        string directory = Directory.GetCurrentDirectory();
+                        Console.WriteLine(site[i].ToString());
+                        string html = client.DownloadString(site[i].ToString());
+                        File.WriteAllText(directory + @"\" + i + ".html", html);
+                        Console.WriteLine("File save");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error : " + e.ToString());
+                }
+            }
+        }
+    }
+}
